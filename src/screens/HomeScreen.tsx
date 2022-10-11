ff5a7dfc086058f722ab415b1e61f3d4411e25f0
Giorgi Darbinyan <darbinian97@gmail.com>
2022-10-11 14:26:34 +0400

    last update

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useCallback, useMemo, useRef } from 'react'
+import React, { useState, useRef } from 'react';
 import {
   StyleSheet,
   Text,
@@ -8,12 +8,12 @@ import {
   Dimensions,
   TouchableOpacity,
   Modal,
-  Pressable
-} from 'react-native'
-import { useQuery, gql } from '@apollo/client'
-import { DisplayImageOrVideo } from '../components/DisplayImageOrVideo'
-import AddCharacter, { fileType } from './AddCharacter'
-import Icon from 'react-native-vector-icons/MaterialCommunityIcons'
+  Pressable,
+} from 'react-native';
+import { useQuery, gql } from '@apollo/client';
+import { DisplayImageOrVideo } from '../components/DisplayImageOrVideo';
+import { fileType } from './AddCharacter';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 
 export const GET_CHARACTERS = gql`
   query {
@@ -28,28 +28,33 @@ export const GET_CHARACTERS = gql`
       }
     }
   }
-`
-const windowWidth = Dimensions.get('window').width
-const windowHeight = Dimensions.get('window').height
+`;
 
-const HomeScreen = ({ navigation }: { navigation: any }): JSX.Element => {
-  const [modalVisible, setModalVisible] = useState(false)
-  const ref = useRef(null)
-  const [currentSlideIndex, setCurrentSlideIndex] = useState(0)
+const windowWidth = Dimensions.get('window').width;
+const windowHeight = Dimensions.get('window').height;
 
-  const [item, setItem] = useState(null)
+const HomeScreen = ({ navigation }: { navigation: any }) => {
+  const [modalVisible, setModalVisible] = useState(false);
+  const ref = useRef(null);
+  const [, setCurrentSlideIndex] = useState(0);
+
+  const [item, setItem] = useState(null);
 
   const udpateCurrentSlideIndex = (e: any) => {
-    const contentOffsetY = e.nativeEvent.contentOffset.y
-    const currentIndex = Math.round(contentOffsetY / windowHeight)
-    setCurrentSlideIndex(currentIndex)
+    const contentOffsetY = e.nativeEvent.contentOffset.y;
+    const currentIndex = Math.round(contentOffsetY / windowHeight);
+    setCurrentSlideIndex(currentIndex);
+  };
+
+  const { error, loading, data } = useQuery(GET_CHARACTERS);
+
+  if (loading) {
+    return <Text>Spinning...</Text>;
   }
 
-  const { error, loading, data } = useQuery(GET_CHARACTERS)
-
-  if (loading) return <Text>Spinning...</Text>
-
-  if (error) return <Text>Something went wrong</Text>
+  if (error) {
+    return <Text>Something went wrong</Text>;
+  }
   return (
     <SafeAreaView>
       <FlatList
@@ -64,17 +69,17 @@ const HomeScreen = ({ navigation }: { navigation: any }): JSX.Element => {
             <Text style={styles.name}>{item.name}</Text>
             <Text style={styles.location}>{item.location.name}</Text>
             <TouchableOpacity
+              // eslint-disable-next-line react-native/no-inline-styles
               style={{ position: 'absolute', top: 20 }}
               onPress={() => {
-                setItem(item)
-                setModalVisible(true)
-              }}
-            >
+                setItem(item);
+                setModalVisible(true);
+              }}>
               <Icon name="menu" size={42} color="#000" />
             </TouchableOpacity>
           </View>
         )}
-        keyExtractor={(item) => item.id}
+        keyExtractor={item => item.id}
       />
       <View style={styles.centeredView}>
         <Modal
@@ -82,28 +87,26 @@ const HomeScreen = ({ navigation }: { navigation: any }): JSX.Element => {
           transparent={true}
           visible={modalVisible}
           onRequestClose={() => {
-            Alert.alert('Modal has been closed.')
-            setModalVisible(!modalVisible)
-          }}
-        >
+            setModalVisible(!modalVisible);
+          }}>
           <View style={styles.centeredView}>
             <View style={styles.modalView}>
               <TouchableOpacity
                 style={styles.addBtn}
                 onPress={() => {
-                  navigation.navigate('AddCharacter', { id: item.id })
-                  setModalVisible(!modalVisible)
-                }}
-              >
+                  // @ts-ignore: Object is possibly 'null'
+                  navigation.navigate('AddCharacter', { id: item.id });
+                  setModalVisible(!modalVisible);
+                }}>
                 <Text style={styles.text}>Add</Text>
               </TouchableOpacity>
               <TouchableOpacity
                 style={styles.btn}
                 onPress={() => {
-                  navigation.navigate('EditCharacter', { id: item.id })
-                  setModalVisible(!modalVisible)
-                }}
-              >
+                  // @ts-ignore: Object is possibly 'null'
+                  navigation.navigate('EditCharacter', { id: item.id });
+                  setModalVisible(!modalVisible);
+                }}>
                 <Text style={styles.text}>Edit</Text>
               </TouchableOpacity>
               <Pressable style={styles.closeBtn} onPress={() => setModalVisible(!modalVisible)}>
@@ -114,14 +117,14 @@ const HomeScreen = ({ navigation }: { navigation: any }): JSX.Element => {
         </Modal>
       </View>
     </SafeAreaView>
-  )
-}
+  );
+};
 
 const styles = StyleSheet.create({
   list: {
     height: 200,
     width: 200,
-    backgroundColor: 'green'
+    backgroundColor: 'green',
   },
   name: {
     fontSize: 28,
@@ -129,7 +132,7 @@ const styles = StyleSheet.create({
     position: 'absolute',
     bottom: 30,
     left: 20,
-    color: 'black'
+    color: 'black',
   },
   location: {
     fontSize: 22,
@@ -137,7 +140,7 @@ const styles = StyleSheet.create({
     position: 'absolute',
     bottom: 10,
     left: 20,
-    color: 'red'
+    color: 'red',
   },
   btn: {
     height: 40,
@@ -149,12 +152,12 @@ const styles = StyleSheet.create({
     position: 'absolute',
     top: 60,
     left: 10,
-    borderRadius: 8
+    borderRadius: 8,
   },
   text: {
     color: 'white',
     fontSize: 16,
-    fontWeight: 'bold'
+    fontWeight: 'bold',
   },
   addBtn: {
     height: 40,
@@ -166,15 +169,15 @@ const styles = StyleSheet.create({
     position: 'absolute',
     top: 10,
     left: 10,
-    borderRadius: 8
+    borderRadius: 8,
   },
   video: {
     width: windowWidth,
-    height: windowHeight
+    height: windowHeight,
   },
   centeredView: {
     position: 'absolute',
-    top: 20
+    top: 20,
   },
   modalView: {
     // margin: 20,
@@ -189,17 +192,17 @@ const styles = StyleSheet.create({
     shadowColor: '#000',
     shadowOffset: {
       width: 0,
-      height: 2
+      height: 2,
     },
     shadowOpacity: 0.25,
     shadowRadius: 4,
-    elevation: 5
+    elevation: 5,
   },
   closeBtn: {
     position: 'absolute',
     top: 0,
-    right: 0
-  }
-})
+    right: 0,
+  },
+});
 
 export default HomeScreen;
